688ebcb8a0731bb0c2012607be1bd2e2341b7be1
J-Shearer <jeanne.shearer@duke.edu>
2018-12-03 14:25:44 -0500

    updated R scripts

--- src2/readData.R
+++ src2/readData.R
@@ -1,17 +1,22 @@
 # This is a script to read data 
 
+library(dplyr)
+library(readr)
+library(fields)
+library(lme4)
+library(ggplot2)
 # read in csv files
 # positional data about the RV Kahuna
-kahuna<-read_csv('2018-11-26_2017-Cape-Hatteras-BRS-kahuna-CEE')
+kahuna<-read.csv('../data/2018-11-26_2017-Cape-Hatteras-BRS-kahuna-CEE')
 kStart <- kahuna %>% 
   filter(status == 'start')
 
 # Read in Gm182 Data: 100 estimated positions of Gm182, augmented with focal follow data
-gm182UP <- read_csv('2018-11-27_Gm182-UserPoints-Start-CEE-Locations-Kahuna.csv') %>% 
+gm182UP <- read_csv('../data/2018-11-27_Gm182-UserPoints-Start-CEE-Locations-Kahuna.csv') %>% 
   mutate(status = 'userPoints')
 
 # Read in Gm182 Data: 100 estimated positions of Gm182
-gm182 <- read_csv('2018-11-27_Gm182-Start-CEE-Locations-Kahuna.csv') %>% 
+gm182 <- read_csv('../data/2018-11-27_Gm182-Start-CEE-Locations-Kahuna.csv') %>% 
   mutate(status = 'noUserPoints')
 
 
--- src2/runALL.R
+++ src2/runALL.R
@@ -1,2 +1,12 @@
 # Script to execute all of the other scripts
 #
+
+source("../src2/readData.R")
+
+source("../src2/wrangle.R")
+
+source("../src2/plot.R")
+
+source("../src2/calcd2ship.R")
+
+source("../src2/regression.R")
